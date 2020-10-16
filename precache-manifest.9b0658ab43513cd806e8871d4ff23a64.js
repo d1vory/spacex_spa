1b5d22c07f80fd7a2520a02e369a4a68d397253e
D1VORY <dubchakob@gmail.com>
2020-10-16 23:17:23 +0300

    Updates

---
+++ precache-manifest.9b0658ab43513cd806e8871d4ff23a64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd7dad316ae55afd6395003686cd80a2",
+    "url": "/spacex_spa/index.html"
+  },
+  {
+    "revision": "17d90f6ad07c601fad22",
+    "url": "/spacex_spa/static/css/main.3f9cc228.chunk.css"
+  },
+  {
+    "revision": "6e32512f237b7df20588",
+    "url": "/spacex_spa/static/js/2.a8fe6547.chunk.js"
+  },
+  {
+    "revision": "8a9adb48f7af2062183ac70b4922c4ef",
+    "url": "/spacex_spa/static/js/2.a8fe6547.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17d90f6ad07c601fad22",
+    "url": "/spacex_spa/static/js/main.d14fe291.chunk.js"
+  },
+  {
+    "revision": "e95cc2403bbdc9fb7c11",
+    "url": "/spacex_spa/static/js/runtime-main.958aa4ab.js"
+  },
+  {
+    "revision": "d5ab31ce033fc418fd1af87f7b934e1b",
+    "url": "/spacex_spa/static/media/logo.d5ab31ce.svg"
+  }
+]);
